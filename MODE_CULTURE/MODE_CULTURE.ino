6e7ecdce7013d666c9df8a616e2ef1c295b36e1b
El-castor <clementpch@gmail.com>
2018-11-06 22:35:12 +0100

    Add sample Ec meter to final version, worked well

--- MODE_CULTURE/MODE_CULTURE.ino
+++ MODE_CULTURE/MODE_CULTURE.ino
@@ -6,13 +6,21 @@
 #include <Time.h>//Inclusion librarie Time
 #include <DHT.h>
 #include <LiquidCrystal.h>
+#include "DFRobot_EC.h"
+#include <EEPROM.h>
+#include <OneWire.h>
 
 #define DHTPIN A0 // pin du DHT
 #define DHTTYPE DHT22 // type de dht
+//#define EC_PIN A8
+//float voltage,voltageBeforeTreatment,ecValue,temperature = 25;
+//DFRobot_EC ec;
 
 #define PHPIN 7          
-
 DHT dht(DHTPIN, DHTTYPE); // test du fonctionnement
+
+#define StartConvert 0
+#define ReadTemperature 1
 
 unsigned long int avgValue;  //Store the average value of the sensor feedback
 float b;
@@ -25,7 +33,20 @@ int brasseur = 10; //Ventillo brasseur introduit en pin 10 (numérique)
 int lampe = 9; //Lampe introduit en pin 9 (numérique)
 int chauffage = 8; //Chauffage introduit en pin 8 (numérique)
 
+const byte numReadings = 20;     //the number of sample times
+byte ECsensorPin = A8;  //EC Meter analog output,pin on analog 1
+byte DS18B20_Pin = 43; //DS18B20 signal, pin on digital 2
+unsigned int AnalogSampleInterval=25,printInterval=700,tempSampleInterval=850;  //analog sample interval;serial print interval;temperature sample interval
+unsigned int readings[numReadings];      // the readings from the analog input
+byte index = 0;                  // the index of the current reading
+unsigned long AnalogValueTotal = 0;                  // the running total
+unsigned int AnalogAverage = 0,averageVoltage=0;                // the average
+unsigned long AnalogSampleTime,printTime,tempSampleTime;
+float temperature,ECcurrent; 
+
 LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
+//Temperature chip i/o
+OneWire ds(DS18B20_Pin);  // on digital pin 2
 
 char val;
 
@@ -56,9 +77,18 @@ void setup()  //Fonction d'initialisation de la carte
   digitalWrite(extracteur, LOW);  //écriture en sortie (broche 13) d'un état BAS
   digitalWrite(intracteur, LOW); //écriture en sortie (broche 12) d'un état BAS
 
+  // initialize all the readings to 0:
+  for (byte thisReading = 0; thisReading < numReadings; thisReading++)
+    readings[thisReading] = 0;
+  TempProcess(StartConvert);   //let the DS18B20 start the convert
+  AnalogSampleTime=millis();
+  printTime=millis();
+  tempSampleTime=millis();
+
   //initialize serial communications at a 9600 baud rate
   Serial.begin(9600);
   lcd.begin(20,4); //initialisation du lcd de verif fonctionnement automate sans interface
+  //ec.begin();
 }
 
 //Nouvelles fonctions qui s'affiche "choix des modes de cultures"***********************************************************************************
@@ -402,11 +432,76 @@ void loop()
     avgValue+=buf[i];
   float phValue=(float)avgValue*5.0/1024/6; //convert the analog into millivolt
   phValue=3.5*phValue;                      //convert the millivolt into pH value
-  digitalWrite(13, HIGH);       
-  delay(800);
-  digitalWrite(13, LOW); 
+  //digitalWrite(13, HIGH);       
+  //delay(800);
+  //digitalWrite(13, LOW); 
 
   //Fin du sample du pH metre
+
+  //Début du sample de l'Ec metre
+ /*
+   Every once in a while,sample the analog value and calculate the average.
+  */
+  if(millis()-AnalogSampleTime>=AnalogSampleInterval)  
+  {
+    AnalogSampleTime=millis();
+     // subtract the last reading:
+    AnalogValueTotal = AnalogValueTotal - readings[index];
+    // read from the sensor:
+    readings[index] = analogRead(ECsensorPin);
+    // add the reading to the total:
+    AnalogValueTotal = AnalogValueTotal + readings[index];
+    // advance to the next position in the array:
+    index = index + 1;
+    // if we're at the end of the array...
+    if (index >= numReadings)
+    // ...wrap around to the beginning:
+    index = 0;
+    // calculate the average:
+    AnalogAverage = AnalogValueTotal / numReadings;
+  }
+  /*
+   Every once in a while,MCU read the temperature from the DS18B20 and then let the DS18B20 start the convert.
+   Attention:The interval between start the convert and read the temperature should be greater than 750 millisecond,or the temperature is not accurate!
+  */
+   if(millis()-tempSampleTime>=tempSampleInterval) 
+  {
+    tempSampleTime=millis();
+    temperature = TempProcess(ReadTemperature);  // read the current temperature from the  DS18B20
+    TempProcess(StartConvert);                   //after the reading,start the convert for next reading
+  }
+   /*
+   Every once in a while,print the information on the serial monitor.
+  */
+  if(millis()-printTime>=printInterval)
+  {
+    printTime=millis();
+    averageVoltage=AnalogAverage*(float)5000/1024;
+    Serial.print("Analog value:");
+    Serial.print(AnalogAverage);   //analog average,from 0 to 1023
+    Serial.print("    Voltage:");
+    Serial.print(averageVoltage);  //millivolt average,from 0mv to 4995mV
+    Serial.print("mV    ");
+    Serial.print("temp:");
+    Serial.print(temperature);    //current temperature
+    Serial.print("^C     EC:");
+    
+    float TempCoefficient=1.0+0.0185*(temperature-25.0);    //temperature compensation formula: fFinalResult(25^C) = fFinalResult(current)/(1.0+0.0185*(fTP-25.0));
+    float CoefficientVolatge=(float)averageVoltage/TempCoefficient;   
+    if(CoefficientVolatge<150)Serial.println("No solution!");   //25^C 1413us/cm<-->about 216mv  if the voltage(compensate)<150,that is <1ms/cm,out of the range
+    else if(CoefficientVolatge>3300)Serial.println("Out of the range!");  //>20ms/cm,out of the range
+    else
+    { 
+      if(CoefficientVolatge<=448)ECcurrent=6.84*CoefficientVolatge-0,7;   //1ms/cm<EC<=3ms/cm 
+      else if(CoefficientVolatge<=1457)ECcurrent=6.98*CoefficientVolatge-127;  //3ms/cm<EC<=10ms/cm
+      else ECcurrent=5.3*CoefficientVolatge+2278;                           //10ms/cm<EC<20ms/cm
+      ECcurrent/=1000;    //convert us/cm to ms/cm
+      Serial.print(ECcurrent,2);  //two decimal
+      Serial.println("ms/cm");
+    }
+  }
+  
+//fin du sample de l'Ec metre
 
   // If data is available to read,
   if (Serial.available() > 0) 
@@ -430,8 +525,52 @@ void loop()
     Serial.print(digitalRead(brasseur));
     Serial.println();
   }
- {
+ /*{
   delay(1000);
 
-  }
+  }*/
 }
+
+/*
+ch=0,let the DS18B20 start the convert;ch=1,MCU read the current temperature from the DS18B20.
+*/
+float TempProcess(bool ch)
+{
+  //returns the temperature from one DS18B20 in DEG Celsius
+  static byte data[12];
+  static byte addr[8];
+  static float TemperatureSum;
+  if(!ch){
+          if ( !ds.search(addr)) {
+              Serial.println("no more sensors on chain, reset search!");
+              ds.reset_search();
+              return 0;
+          }      
+          if ( OneWire::crc8( addr, 7) != addr[7]) {
+              Serial.println("CRC is not valid!");
+              return 0;
+          }        
+          if ( addr[0] != 0x10 && addr[0] != 0x28) {
+              Serial.print("Device is not recognized!");
+              return 0;
+          }      
+          ds.reset();
+          ds.select(addr);
+          ds.write(0x44,1); // start conversion, with parasite power on at the end
+  }
+  else{  
+          byte present = ds.reset();
+          ds.select(addr);    
+          ds.write(0xBE); // Read Scratchpad            
+          for (int i = 0; i < 9; i++) { // we need 9 bytes
+            data[i] = ds.read();
+          }         
+          ds.reset_search();           
+          byte MSB = data[1];
+          byte LSB = data[0];        
+          float tempRead = ((MSB << 8) | LSB); //using two's compliment
+          TemperatureSum = tempRead / 16;
+    }
+          return TemperatureSum;  
+}
+
